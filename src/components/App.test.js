f4a48f1573dad066550019337f5396645e57eca6
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-03 21:16:26 +0100

    Refactor tests to use describe, beforEach and afterEach methods

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,21 +4,30 @@ import App from "./App";
 
 const app = shallow(<App />);
 
-it("renders correctly", () => {
-  expect(app).toMatchSnapshot();
-});
+describe("App", () => {
+  it("renders correctly", () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it("initializes the `state` with an empty list of gifts", () => {
-  expect(app.state().gifts).toEqual([]);
-});
+  it("initializes the `state` with an empty list of gifts", () => {
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it("adds a new gift to the `state` after clicking the `add gift` button", () => {
-  app.find(".btn-add").simulate("click");
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+  describe("when clicking the `add-gift` button", () => {
+    beforeEach(() => {
+      app.find(".btn-add").simulate("click");
+    });
 
-it("adds a new gift to the rendered list when clicking the `add gift` button", () => {
-  app.find(".btn-add").simulate("click");
+    afterEach(() => {
+      app.setState({ gifts: [] });
+    });
 
-  expect(app.find(".gift-list").children().length).toEqual(2);
+    it("adds a new gift to the `state`", () => {
+      expect(app.state().gifts).toEqual([{ id: 1 }]);
+    });
+
+    it("adds a new gift to the rendered list", () => {
+      expect(app.find(".gift-list").children().length).toEqual(1);
+    });
+  });
 });
